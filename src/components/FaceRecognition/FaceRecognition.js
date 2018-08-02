b85b9e69d26a7a54ba24562ac56468b207fdadf3
2jiwon <ljwjulian@gmail.com>
2018-08-03 00:59:11 +0900

    Test the Clarifai API COLOR_MODEL for our FaceRecognition component.

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const FaceRecognition = () => {
   return (
-    <div>
+    <div className='center'>
+      <img src={'https://samples.clarifai.com/face-det.jpg'} />
     </div>
   );
 }
