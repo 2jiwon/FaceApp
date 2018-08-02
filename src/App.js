5324273bc7b282d3cbfab50e39ef4dcd2ec68054
2jiwon <ljwjulian@gmail.com>
2018-08-02 22:10:43 +0900

    We can now detect button click.

--- src/App.js
+++ src/App.js
@@ -36,6 +36,10 @@ class App extends Component {
     console.log(event.target.value);
   }
 
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render () {
     return (
       <div className='App'>
@@ -43,7 +47,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} />
         { /*
           <FaceRecognition /> */ }
       </div>
