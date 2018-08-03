e55fab816cff7d5623c26158c7f4c23fa9e32200
2jiwon <ljwjulian@gmail.com>
2018-08-03 22:55:03 +0900

    Added a image variable for manipulating with the DOM.

--- src/App.js
+++ src/App.js
@@ -42,6 +42,10 @@ class App extends Component {
   
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+           const image = document.getElementById('inputImage');
+           const width = Number(image.width);
+          const height = Number(image.height);
+    console.log(width, height);
   }
 
   onInputChange = (event) => {
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({ imageUrl }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img src={imageUrl} width='500px' height='auto' />
+        <img alt='inputImage' id='inputImage' src={imageUrl} width='500px' height='auto' />
       </div>
     </div>
   );
