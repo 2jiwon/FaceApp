9e844c2928d39b88b697a2cbbc6cbbab7594a8ab
2jiwon <ljwjulian@gmail.com>
2018-08-03 22:56:48 +0900

    Added return object calculating the bounding box area.

--- src/App.js
+++ src/App.js
@@ -46,6 +46,12 @@ class App extends Component {
            const width = Number(image.width);
           const height = Number(image.height);
     console.log(width, height);
+    return {
+        leftCol: clarifaiFace.left_col * width,
+         topRow: clarifaiFace.top_row  * height,
+       rightCol:  width - (clarifaiFace.right_col  * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
 
   onInputChange = (event) => {
